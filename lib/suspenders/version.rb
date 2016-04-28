99bc87a6ae9df0e9d46202b49677ccdb74fb5595
Tute Costa <tutecosta@gmail.com>
2016-04-28 17:38:43 -0400

    Upgrade to Rails 5.0.0.beta3
    In preparation for Rails 5.
    
    I needed to add the `listen` gem in development. See related PR to
    Rails and its related issues: https://github.com/rails/rails/pull/24243
    
    Right now, generating an app shows:
    
    ```
    conflict  config/puma.rb
    Overwrite test-apr-28/config/puma.rb? (enter "h" for help) [Ynaqdh]
    ```

--- lib/suspenders/version.rb
+++ lib/suspenders/version.rb
@@ -1,5 +1,5 @@
 module Suspenders
-  RAILS_VERSION = "~> 4.2.0"
+  RAILS_VERSION = "~> 5.0.0.beta4"
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
   VERSION = "1.38.1"
 end
